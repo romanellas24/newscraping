b9da97080048bc974f97994c16138ecedff5e3f5
Daniele Romanella <www.daniele.romanella@gmail.com>
2024-03-20 17:49:16 +0100

    Tutti gli scraper sono stati rivisitati

--- scrapyGetters/scrapyGetters/spiders/dailyscrape.sh
+++ scrapyGetters/scrapyGetters/spiders/dailyscrape.sh
@@ -1,11 +1,11 @@
 #!/bash/sh
 PATH=$PATH:/home/students/giuseppe.carrino2/.local/bin
 export PATH
-scrapy crawl zdfGet # OK
-scrapy crawl rtsURLGet # OK
-scrapy crawl rtsGet # OK
-scrapy crawl pbsGet # OK
-scrapy crawl frGet # OK
+scrapy crawl zdfGet
+scrapy crawl rtsURLGet
+scrapy crawl rtsGet
+scrapy crawl pbsGet
+scrapy crawl frGet
 scrapy crawl 20Get
 # scrapy crawl gr1url
 # scrapy crawl gr1Get
