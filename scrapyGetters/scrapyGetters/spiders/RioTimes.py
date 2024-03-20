d1578cd858a05d6434057b75d2cfe3f0f878d134
Daniele Romanella <www.daniele.romanella@gmail.com>
2024-03-20 19:02:19 +0100

    RioTimes è stato integrato a tutti gli altri Scraper nel dailyScrape.sh!

--- scrapyGetters/scrapyGetters/spiders/RioTimes.py
+++ scrapyGetters/scrapyGetters/spiders/RioTimes.py
@@ -1,9 +1,18 @@
+from os import path
+from typing import Union
+
 import scrapy
 from datetime import date
 import time
+import dateparser
+from scrapy import Spider
+from twisted.internet.defer import Deferred
+import json
 
+SCRIPTS_DIR = path.dirname(__file__)
+PROJ_DIR = f"{SCRIPTS_DIR}/../../../"
 
-class QuoteSpider(scrapy.Spider):
+class RioTimes(scrapy.Spider):
     name = "RioTimes"
 
     start_urls = [
@@ -11,6 +20,7 @@ class QuoteSpider(scrapy.Spider):
     ]
     referred_link = ''
     ranked = 0
+    edition = []
 
     def parse(self, response):
         for news in response.xpath("//a[@rel='bookmark' and not(contains(@class, 'td-image-wrap '))]"):
@@ -25,11 +35,26 @@ class QuoteSpider(scrapy.Spider):
                 self.referred_link = subpage_link
                 yield response.follow(subpage_link, self.parse)
 
+    def close(spider: Spider, reason: str) -> Union[Deferred, None]:
+        if reason == 'finished':
+            base_name = f"{str(spider.edition[0]['date'])}.json"
+            for new in spider.edition:
+                new['date'] = str(new['date'])
+            scraped_data_dir = f"{PROJ_DIR}/collectedNews/flow/BR/RioTimes"
+            scraped_data_filepath = f"{scraped_data_dir}/{base_name}"
+            with open(scraped_data_filepath, "w") as f:
+                json.dump(spider.edition, f, indent=4, ensure_ascii=False)
+                f.write("\n")
+        return spider.close(spider, reason)
+
     def parseArticle(self, response):
         parent_url = response.meta['parent']
         title = response.xpath("//h1[@class = 'tdb-title-text']/text()").get()
         today = date.today()
         date_raw = response.xpath("//div[@class = 'tdb-block-inner td-fix-index']/time/@datetime").get()
+        date_parsed = dateparser.parse(date_raw)
+        if date_parsed.date() != today:
+            pass
         news_url = response.request.url
         content_paragraph = response.xpath('//*[contains(@class, "tdb_single_content")]//p')
         content = ''
@@ -41,7 +66,7 @@ class QuoteSpider(scrapy.Spider):
             p_content = p_content.replace('To read the full NEWS and much more,', '')
             content = content + "\n" + p_content
 
-        yield {
+        new = {
             'title': title,
             'date_raw': date_raw,  # Directly from the document
             'date': today,
@@ -55,3 +80,4 @@ class QuoteSpider(scrapy.Spider):
             'language': 'EN',
             'source': 'Rio Times'
         }
+        self.edition.append(new)
